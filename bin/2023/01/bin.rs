bf3eed6762248216e88ee6ce846f12ea07305d45
MingXu <91617361+M1ngXU@users.noreply.github.com>
2023-12-01 07:18:39 +0100

    refactor

--- bin/2023/01/bin.rs
+++ bin/2023/01/bin.rs
@@ -5,20 +5,18 @@ const DIGITS: [&str; 20] = [
     "3", "4", "5", "6", "7", "8", "9",
 ];
 
-fn one() {
-    let reg_first = r!(&format!(".*?({})", DIGITS[10..].join("|")));
-    let reg_last = r!(&format!(".*({})", DIGITS[10..].join("|")));
-    include_str!("example1.txt")
+fn process(rf: Regex, rl: Regex, input: &str) {
+    input
         .lines()
         .map(|s| {
             let first = DIGITS
                 .into_iter()
-                .position(|d| d == reg_first.captures(s).unwrap().get(1).unwrap().as_str())
+                .position(|d| d == rf.captures(s).unwrap().get(1).unwrap().as_str())
                 .unwrap()
                 % 10;
             let last = DIGITS
                 .into_iter()
-                .position(|d| d == reg_last.captures(s).unwrap().get(1).unwrap().as_str())
+                .position(|d| d == rl.captures(s).unwrap().get(1).unwrap().as_str())
                 .unwrap()
                 % 10;
             first * 10 + last
@@ -27,26 +25,18 @@ fn one() {
         .save();
 }
 
+fn one() {
+    let reg_first = r!(&format!(".*?({})", DIGITS[10..].join("|")));
+    let reg_last = r!(&format!(".*({})", DIGITS[10..].join("|")));
+    let input = include_str!("example1.txt");
+    process(reg_first, reg_last, input);
+}
+
 fn two() {
     let reg_first = r!(&format!(".*?({})", DIGITS.join("|")));
     let reg_last = r!(&format!(".*({})", DIGITS.join("|")));
-    include_str!("example2.txt")
-        .lines()
-        .map(|s| {
-            let first = DIGITS
-                .into_iter()
-                .position(|d| d == reg_first.captures(s).unwrap().get(1).unwrap().as_str())
-                .unwrap()
-                % 10;
-            let last = DIGITS
-                .into_iter()
-                .position(|d| d == reg_last.captures(s).unwrap().get(1).unwrap().as_str())
-                .unwrap()
-                % 10;
-            first * 10 + last
-        })
-        .s()
-        .save();
+    let input = include_str!("example2.txt");
+    process(reg_first, reg_last, input);
 }
 
 fn main() {
