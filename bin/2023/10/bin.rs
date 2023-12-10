615dbf8962f88e4f6a841bbdfba7ee5cc1ca1831
MingXu <91617361+M1ngXU@users.noreply.github.com>
2023-12-10 14:33:12 +0100

    index on main: 3e9f84f s

--- bin/2023/10/bin.rs
+++ bin/2023/10/bin.rs
@@ -108,16 +108,13 @@ fn two() {
                 .collect_vec()
         })
         .collect_vec();
-    println!("{m:?}");
     m.insert(0, vec![vec![]; m[0].len()]);
     m.push(vec![vec![]; m[0].len()]);
     for r in m.iter_mut() {
         r.insert(0, vec![]);
         r.push(vec![]);
     }
-    println!("{m:?}");
     let m2 = m.clone();
-    assert_eq!(m, m2);
 
     let mm = m
         .into_iter()
@@ -133,40 +130,21 @@ fn two() {
                             .unwrap();
 
                         let mut out = vec![];
-                        // println!(
-                        //     "{x}|{y}|{}|{}|{:?}|{:?}|{:?}|{:?}|{}|{}|{}|{}",
-                        //     x + 1,
-                        //     y + 2,
-                        //     m2.get((y + 0, x + 1)).unwrap(),
-                        //     m2.get((y + 1, x + 0)).unwrap(),
-                        //     m2.get((y + 1, x + 2)).unwrap(),
-                        //     m2.get((y + 2, x + 1)).unwrap(),
-                        //     s2.get((y - 1, x + 0)).unwrap(),
-                        //     s2.get((y + 0, x - 1)).unwrap(),
-                        //     s2.get((y + 0, x + 1)).unwrap(),
-                        //     s2.get((y + 1, x + 0)).unwrap(),
-                        // );
                         if m2[y][x + 1].contains(&(0, 1)) {
                             out.push((0, -1));
-                            println!("{out:?}");
                         }
 
                         if m2[y + 1][x].contains(&(1, 0)) {
                             out.push((-1, 0));
-                            println!("{out:?}");
                         }
 
                         if m2[y + 1][x + 2].contains(&(-1, 0)) {
                             out.push((1, 0));
-                            println!("{out:?}");
                         }
 
                         if m2[y + 2][x + 1].contains(&(0, -1)) {
                             out.push((0, 1));
-                            println!("{out:?}");
                         }
-
-                        println!("out: {out:?}|{x}|{y}");
                         out
                     }
                     _ => x,
@@ -183,8 +161,6 @@ fn two() {
     let x = x + 1; // padding
     let y = y + 1;
 
-    println!("{mm:?}");
-
     let reachable = dijkstraa(
         (0, (x as isize, y as isize)),
         |c, (x, y)| {
@@ -196,7 +172,7 @@ fn two() {
         },
         |_, _| 0,
     );
-    println!("fff {reachable:?}");
+
     let mut mmm = mm
         .iter()
         .map(|r| r.iter().map(|_| vec![]).collect_vec())
@@ -204,94 +180,41 @@ fn two() {
     for (_, (x, y)) in reachable {
         mmm[y as usize][x as usize] = mm[y as usize][x as usize].clone();
     }
-    println!("{mmm:?}");
 
     let mut inside = 0;
-    for (x, y) in (1..mmm[0].len() - 1).cartesian_product(1..mmm.len() - 1) {
-        if mmm[y][x] != vec![] || s[y - 1][x - 1] != '.' {
-            continue;
-        }
-        // println!("{x}|{y}");
-        let mut ins: bool = true;
-        let mut inv = 0;
-
-        let mut counter = 0;
-        for xx in (0..x).rev() {
-            if mmm[y][xx].contains(&(1, 0)) || mmm[y][xx].contains(&(-1, 0)) {
-                counter = 1;
-                if xx == x - 1 {
-                    inv += 1;
+    'outer: for (x, y) in (1..mmm[0].len() - 1)
+        .cartesian_product(1..mmm.len() - 1)
+        .filter(|(x, y)| mmm[*y][*x].is_empty())
+    {
+        for dx in -1..=1 {
+            for dy in -1..=1 {
+                if (dx != 0 && dy != 0) {
+                    //(dx == 0 || dy == 0) && dx != dy {
+                    let mut xx = x as isize;
+                    let mut yy = y as isize;
+                    let mut inside = 0;
+                    while (1..mmm[0].len() - 1).contains(&(xx as usize))
+                        && (1..mmm.len() - 1).contains(&(yy as usize))
+                    {
+                        let non_blocking = vec![(-dx, 0), (0, dy), (0, -dy), (dx, 0)];
+                        if !(mmm[yy as usize][xx as usize].contains(&non_blocking[0])
+                            && mmm[yy as usize][xx as usize].contains(&non_blocking[1])
+                            || mmm[yy as usize][xx as usize].contains(&non_blocking[2])
+                                && mmm[yy as usize][xx as usize].contains(&non_blocking[3]))
+                            && !mmm[yy as usize][xx as usize].is_empty()
+                        {
+                            inside += 1;
+                        }
+                        xx += dx;
+                        yy += dy;
+                    }
+                    if inside % 2 == 0 {
+                        continue 'outer;
+                    }
                 }
-                break;
-            }
-            if mmm[y][xx].contains(&(0, 1)) || mmm[y][xx].contains(&(0, -1)) {
-                counter += 1;
             }
         }
-        if counter % 2 == 0 {
-            // println!("x|{counter}");
-            ins = false;
-        }
-        let mut counter = 0;
-        for xx in x + 1..mmm[0].len() - 1 {
-            if mmm[y][xx].contains(&(1, 0)) || mmm[y][xx].contains(&(-1, 0)) {
-                counter = 1;
-                if xx == x + 1 {
-                    inv += 1;
-                }
-                break;
-            }
-            if mmm[y][xx].contains(&(0, 1)) || mmm[y][xx].contains(&(0, -1)) {
-                counter += 1;
-            }
-        }
-        if counter % 2 == 0 {
-            // println!("xx");
-            ins = false;
-        }
-        let mut counter = 0;
-        for yy in (0..y).rev() {
-            if mmm[yy][x].contains(&(0, 1)) || mmm[yy][x].contains(&(0, -1)) {
-                counter = 1;
-                if yy == y - 1 {
-                    inv += 1;
-                }
-                break;
-            }
-            if mmm[yy][x].contains(&(1, 0)) || mmm[yy][x].contains(&(-1, 0)) {
-                counter += 1;
-            }
-        }
-        if counter % 2 == 0 {
-            // println!("y");
-            ins = false;
-        }
-        let mut counter = 0;
-        for yy in y + 1..mmm.len() - 1 {
-            if mmm[yy][x].contains(&(0, 1)) || mmm[yy][x].contains(&(0, -1)) {
-                counter = 1;
-                if yy == y + 1 {
-                    inv += 1;
-                }
-                break;
-            }
-            if mmm[yy][x].contains(&(1, 0)) || mmm[yy][x].contains(&(-1, 0)) {
-                counter += 1;
-            }
-        }
-        if counter % 2 == 0 {
-            // println!("yy");
-            ins = false;
-        }
-        let mut i = false;
-        for d in 0..x.min(y) {
-            let dx = -(d as isize);
-            if mmm[dy + y][dx + x] {}
-        }
-        if ins {
-            println!("here: {x}|{y}|{inv}");
-            inside += 1;
-        }
+        inside += 1;
     }
     inside.save();
 }
