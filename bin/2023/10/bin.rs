a19d7599445c1b417866a2f8423097b0b8de6066
MingXu <91617361+M1ngXU@users.noreply.github.com>
2023-12-10 16:36:53 +0100

    2.5ms

--- bin/2023/10/bin.rs
+++ bin/2023/10/bin.rs
@@ -1,269 +1,106 @@
 #![feature(iter_repeat_n, iter_map_windows, iter_from_coroutine)]
 
+use std::time::Instant;
+
 use aoc_rs::prelude::*;
 
-fn one() {
-    let p = grd;
-    let s = pi!(p);
-    let s2 = s.clone();
+const TABLE: [((isize, isize), (isize, isize), bool); 13] = [
+    ((0, 0), (0, 0), false),
+    ((0, 0), (0, 0), false),
+    ((0, 0), (0, 0), false),
+    ((0, 1), (1, 0), true), // F
+    ((0, 0), (0, 0), false),
+    ((0, 1), (-1, 0), false), // 7
+    ((1, 0), (-1, 0), false), // -
+    ((0, 0), (0, 0), false),
+    ((0, 0), (0, 0), false),
+    ((0, -1), (0, 1), false), // |
+    ((0, -1), (1, 0), false), // L
+    ((0, 0), (0, 0), false),
+    ((0, -1), (-1, 0), true), // J
+];
 
-    let m: Matrix<Vec<(isize, isize)>, Dyn, Dyn, _> = s.map(|x| match x {
-        '|' => vec![(0, 1), (0, -1)],
-        '-' => vec![(1, 0), (-1, 0)],
-        'L' => vec![(0, -1), (1, 0)],
-        'J' => vec![(0, -1), (-1, 0)],
-        '7' => vec![(0, 1), (-1, 0)],
-        'F' => vec![(0, 1), (1, 0)],
-        '.' => vec![],
-        'S' => {
-            vec![(-1, -1)]
-        }
-        _ => unreachable!(),
-    });
-    let m = m.pad(vec![]);
-    let m2 = m.clone();
-
-    let mm = m.map(|x| match x.first() {
-        Some(&(-1, -1)) => {
-            let (x, y, _) = s2.enumerate_iter().find(|(_, _, c)| c == &&'S').unwrap();
-            let b = m2.adj(x + 1, y + 1, 1);
-            println!("{:?}", b.iter().collect_vec());
-            let mut iter = b.iter();
-            iter.next(); // top left
-            let mut out = vec![];
-            if iter.next().unwrap().contains(&(0, 1)) {
-                out.push((0, -1));
-                println!("{out:?}");
-            }
-            iter.next(); // top right
-            if iter.next().unwrap().contains(&(1, 0)) {
-                out.push((-1, 0));
-                println!("{out:?}");
-            }
-            iter.next(); // self
-            if iter.next().unwrap().contains(&(-1, 0)) {
-                out.push((1, 0));
-                println!("{out:?}");
-            }
-            iter.next(); // bottom left
-            if iter.next().unwrap().contains(&(0, -1)) {
-                out.push((0, 1));
-                println!("{out:?}");
-            }
-            iter.next(); // bottom right
-            assert!(iter.next().is_none());
-            println!("{out:?}");
-            out
-        }
-        _ => x,
-    });
-
-    let (x, y, _) = s2.enumerate_iter().find(|(_, _, c)| c == &&'S').unwrap();
-    let x = x + 1; // padding
-    let y = y + 1;
-
-    println!("{mm:?}");
-
-    dijkstraa(
-        (0, (x as isize, y as isize)),
-        |c, (x, y)| {
-            // println!("{x}|{y}|{c}|{xx}|{yy}");
-            mm.get((*x as usize, *y as usize))
-                .unwrap()
-                .iter()
-                .map(|(dx, dy)| (c + 1, (x + dx, y + dy)))
+fn solve() {
+    let mut s = include_str!("input.txt")
+        .lines()
+        .map(|r| {
+            r.chars()
+                .map(|c| match c {
+                    // top left right bottom
+                    'F' => 0b0011,
+                    '7' => 0b0101,
+                    '-' => 0b0110,
+                    '|' => 0b1001,
+                    'L' => 0b1010,
+                    'J' => 0b1100,
+                    'S' => 0b1111,
+                    _ => 0b0000,
+                })
                 .collect_vec()
-        },
-        |_, _| 0,
-    )
-    .into_iter()
-    .map(|(c, _)| c)
-    .mx()
-    .save();
-}
+        })
+        .collect_vec();
+    let (x, y) = s
+        .iter()
+        .enumerate()
+        .find_map(|(y, r)| r.iter().position(|x| x == &0b1111).map(|x| (x, y)))
+        .unwrap();
 
-fn two() {
-    // let p = parser!((| ch)[LE]);
-    // let mut s = pi!(p);
-    // let s2 = s.clone();
+    s[y][x] = (-1..=1)
+        .cartesian_product(-1..=1)
+        .filter(|(dy, dx)| (dx == &0 || dy == &0) && dx != dy)
+        .zip([0b0001_u8, 0b0010, 0b0100, 0b1000])
+        .filter_map(|((dy, dx), mask)| {
+            let xy = [(x as isize + dx) as usize, (y as isize + dy) as usize];
+            xy.iter()
+                .all(|x| (0..s.len()).contains(x))
+                .then(|| (xy[0], xy[1], mask))
+        })
+        .map(|(x, y, mask)| s[y][x] & mask)
+        .reduce(|a, b| a | b)
+        .unwrap();
 
-    // let mut m = s
-    //     .clone()
-    //     .into_iter()
-    //     .map(|r| {
-    //         r.into_iter()
-    //             .map(|x| match x {
-    //                 '|' => vec![(0, 1), (0, -1)],
-    //                 '-' => vec![(1, 0), (-1, 0)],
-    //                 'L' => vec![(0, -1), (1, 0)],
-    //                 'J' => vec![(0, -1), (-1, 0)],
-    //                 '7' => vec![(0, 1), (-1, 0)],
-    //                 'F' => vec![(0, 1), (1, 0)],
-    //                 '.' => vec![],
-    //                 'S' => {
-    //                     vec![(-1, -1)]
-    //                 }
-    //                 _ => unreachable!(),
-    //             })
-    //             .collect_vec()
-    //     })
-    //     .collect_vec();
-    // m.insert(0, vec![vec![]; m[0].len()]);
-    // m.push(vec![vec![]; m[0].len()]);
-    // for r in m.iter_mut() {
-    //     r.insert(0, vec![]);
-    //     r.push(vec![]);
-    // }
-    // let m2 = m.clone();
-
-    // let mm = m
-    //     .into_iter()
-    //     .map(|r| {
-    //         r.into_iter()
-    //             .map(|x| match x.first() {
-    //                 Some(&(-1, -1)) => {
-    //                     let (x, y) = s2
-    //                         .clone()
-    //                         .into_iter()
-    //                         .enumerate()
-    //                         .find_map(|(y, r)| r.into_iter().position(|c| c == 'S').map(|x| (x, y)))
-    //                         .unwrap();
-
-    //                     let mut out = vec![];
-    //                     if m2[y][x + 1].contains(&(0, 1)) {
-    //                         out.push((0, -1));
-    //                     }
-
-    //                     if m2[y + 1][x].contains(&(1, 0)) {
-    //                         out.push((-1, 0));
-    //                     }
-
-    //                     if m2[y + 1][x + 2].contains(&(-1, 0)) {
-    //                         out.push((1, 0));
-    //                     }
-
-    //                     if m2[y + 2][x + 1].contains(&(0, -1)) {
-    //                         out.push((0, 1));
-    //                     }
-    //                     out
-    //                 }
-    //                 _ => x,
-    //             })
-    //             .collect_vec()
-    //     })
-    //     .collect_vec();
-
-    // let (x, y) = s2
-    //     .into_iter()
-    //     .enumerate()
-    //     .find_map(|(y, r)| r.into_iter().position(|c| c == 'S').map(|x| (x, y)))
-    //     .unwrap();
-    // let x = x + 1; // padding
-    // let y = y + 1;
-
-    let s = pi!(parser!((| ch)[LE]));
-    // let (x, y) = s
-    //     .iter()
-    //     .enumerate()
-    //     .find_map(|(y, r)| r.iter().position(|x| x == &'S').map(|x| (x, y)))
-    //     .unwrap();
-    let (x, y) = (1, 1);
-
-    let mut cx = x;
-    let mut cy = y;
-    let mut ox = x;
-    let mut oy = y;
-    let mut closed_walk = HashMap::new();
+    let (mut cx @ mut ox, mut cy @ mut oy) = (x as isize, y as isize);
+    let mut closed_walk = vec![vec![0; s.len()]; s.len()];
+    let mut len: usize = 0;
     while {
         let c = s[cy as usize][cx as usize];
-        let ((dx1, dy1), (dx2, dy2), i) = match c {
-            '|' => ((0, 1), (0, -1), false),
-            '-' => ((1, 0), (-1, 0), false),
-            'L' => ((0, -1), (1, 0), false),
-            'J' => ((0, -1), (-1, 0), true),
-            '7' => ((0, 1), (-1, 0), false),
-            'F' => ((0, 1), (1, 0), true),
-            _ => unreachable!(),
-        };
+        let ((dx1, dy1), (dx2, dy2), i) = TABLE[c as usize];
         let (oox, ooy) = (cx, cy);
-        if cx + dx1 != ox && cy + dy1 != oy {
-            cx += dx1;
-            cy += dy1;
-        } else {
+        if cx + dx1 == ox && cy + dy1 == oy {
             cx += dx2;
             cy += dy2;
+        } else {
+            cx += dx1;
+            cy += dy1;
         }
         (ox, oy) = (oox, ooy);
-        closed_walk.insert((cx, cy), i);
-        cx != x && cy != y
+        closed_walk[oy as usize][ox as usize] = i as usize + 1;
+        len += 1;
+        cx != x as isize || cy != y as isize
     } {}
 
-    let mut inside = 0;
-    for y in 0..2 * s.len() as isize - 1 {
-        let mut outside = true;
-        for x in (0..=y.min(s.len() as isize - 1)).filter(|x| y - x < s.len() as isize) {
-            match closed_walk.get(&(x, y - x)) {
-                Some(false) => outside = !outside,
-                None => inside += 1,
-                _ => {}
+    println!("Part 1: {}", len.div_ceil(2));
+
+    print!("Part 2: ");
+    (0..2 * s.len() as isize - 1)
+        .map(|y| {
+            let mut inside = 0;
+            let mut outside = true;
+            for x in (0..=y.min(s.len() as isize - 1)).filter(|x| y - x < s.len() as isize) {
+                match closed_walk[(y - x) as usize][x as usize] {
+                    1 => outside = !outside,
+                    0 if !outside => inside += 1,
+                    _ => {}
+                }
             }
-        }
-    }
-
-    inside.save();
-    panic!();
-
-    let reachable = dijkstraa(
-        (0, (x as isize, y as isize)),
-        |c, (x, y)| {
-            // println!("{x}|{y}|{c}|{xx}|{yy}");
-            mm[*y as usize][*x as usize]
-                .iter()
-                .map(|(dx, dy)| (c + 1, (x + dx, y + dy)))
-                .collect_vec()
-        },
-        |_, _| 0,
-    );
-
-    #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-    enum State {
-        Loop,
-        NonBlocking,
-        Ground,
-    }
-
-    let mut mmm = mm
-        .iter()
-        .map(|r| r.iter().map(|_| State::Ground).collect_vec())
-        .collect_vec();
-    for (_, (x, y)) in reachable {
-        if ['F', 'J'].contains(&s[y as usize - 1][x as usize - 1]) {
-            mmm[y as usize][x as usize] = State::NonBlocking;
-        } else {
-            mmm[y as usize][x as usize] = State::Loop;
-        }
-    }
-
-    println!("{mmm:?}");
-
-    let mut inside = 0;
-    for y in 0..2 * mmm.len() - 1 {
-        let mut outside = true;
-        for x in (0..=y.min(mmm.len() - 1)).filter(|x| y - x < mmm.len()) {
-            match mmm[y - x][x] {
-                State::Loop => outside = !outside,
-                State::Ground if !outside => inside += 1,
-                _ => {}
-            }
-        }
-    }
-
-    inside.save();
+            inside
+        })
+        .sum::<usize>()
+        .save();
 }
 
 fn main() {
-    // print!("Part 1: ");
-    // one();
-    print!("Part 2: ");
-    two();
+    let start = Instant::now();
+    solve();
+    println!("Time: {:?}", start.elapsed());
 }
