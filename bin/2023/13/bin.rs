fb54fb81b205de23271c3154582291a3b6b14cd9
MingXu <91617361+M1ngXU@users.noreply.github.com>
2023-12-14 08:13:49 +0100

    14, 65ms

--- bin/2023/13/bin.rs
+++ bin/2023/13/bin.rs
@@ -23,31 +23,39 @@ fn solve() {
 
     let (p1, p2) = s
         .into_iter()
-        .map(|field| {
+        .zip_eq([
+            14, 900, 1200, 1100, 9, 4, 900, 500, 200, 4, 6, 1, 8, 2, 9, 9, 6, 400, 1000, 1100, 400,
+            14, 3, 100, 1500, 400, 2, 300, 10, 10, 400, 5, 500, 14, 2, 200, 500, 8, 4, 900, 2, 2,
+            200, 2, 1, 7, 300, 3, 10, 9, 5, 100, 3, 1, 800, 400, 1400, 12, 400, 1, 100, 14, 200,
+            100, 6, 7, 200, 6, 600, 1100, 5, 10, 8, 2, 4, 4, 11, 900, 1, 2, 2, 10, 700, 9, 5, 8,
+            1100, 8, 2, 200, 5, 5, 500, 4, 800, 700, 4, 600, 6, 900,
+        ])
+        .map(|(field, e)| {
             let valid = findrref(&field)
                 .map(|x| 100 * (x + 1))
                 .next()
                 .unwrap_or_else(|| findrref(&field.clone().t()).next().unwrap() + 1);
-            (
-                valid,
-                field
-                    .ii()
-                    .find_map(|(x, y)| {
-                        let mut fieldcopy = field.clone();
-                        fieldcopy[y][x] = if fieldcopy[y][x] == '#' { '.' } else { '#' };
-                        findrref(&fieldcopy)
-                            .map(|x| 100 * (x + 1))
-                            .filter(|s| s != &valid)
-                            .next()
-                            .or_else(|| {
-                                findrref(&fieldcopy.clone().t())
-                                    .map(|x| x + 1)
-                                    .filter(|x| x != &valid)
-                                    .next()
-                            })
-                    })
-                    .unwrap(),
-            )
+            let nvalid = field
+                .ii()
+                .find_map(|(x, y)| {
+                    let mut fieldcopy = field.clone();
+                    fieldcopy[y][x] = if fieldcopy[y][x] == '#' { '.' } else { '#' };
+                    findrref(&fieldcopy)
+                        .map(|x| 100 * (x + 1))
+                        .filter(|s| s != &valid)
+                        .next()
+                        .or_else(|| {
+                            findrref(&fieldcopy.clone().t())
+                                .map(|x| x + 1)
+                                .filter(|x| x != &valid)
+                                .next()
+                        })
+                })
+                .unwrap();
+            if nvalid != e {
+                println!("{nvalid}|{e}|{:?}", field);
+            }
+            (valid, nvalid)
         })
         .reduce(|(a, b), (c, d)| (a + c, b + d))
         .unwrap();
---
+++ bin/2023/14/bin.rs
@@ -0,0 +1,86 @@
+#![feature(iter_repeat_n, iter_map_windows, iter_from_coroutine)]
+
+use aoc_rs::prelude::*;
+
+fn tilt_north_south(s: &mut Vec<Vec<char>>, dy: isize) {
+    (0..s[0].len()).into_iter().for_each(|x| {
+        let mut mt = if dy > 0 { s.len() as isize - 1 } else { 0 };
+        for i in if dy > 0 {
+            (0..s.len()).rev().collect_vec()
+        } else {
+            (0..s.len()).collect_vec()
+        } {
+            if s[i][x] == '#' {
+                mt = i as isize - dy;
+            } else if s[i][x] == 'O' {
+                s[i][x] = '.';
+                s[mt as usize][x] = 'O';
+                mt -= dy;
+            }
+        }
+    });
+}
+
+fn tilt_east_west(s: &mut Vec<Vec<char>>, dx: isize) {
+    s.iter_mut().for_each(|row| {
+        let mut mt = if dx > 0 { row.len() as isize - 1 } else { 0 };
+        for i in if dx > 0 {
+            (0..row.len()).rev().collect_vec()
+        } else {
+            (0..row.len()).collect_vec()
+        } {
+            if row[i] == '#' {
+                mt = i as isize - dx;
+            } else if row[i] == 'O' {
+                row[i] = '.';
+                row[mt as usize] = 'O';
+                mt -= dx;
+            }
+        }
+    });
+}
+
+fn one() {
+    let p = parser!((|ch)[LE]);
+    let mut s = pi!(p);
+
+    tilt_north_south(&mut s, -1);
+    s.ii()
+        .filter(|(x, y)| s[*y][*x] == 'O')
+        .map(|(_, y)| s.len() - y)
+        .s()
+        .save();
+}
+
+fn two() {
+    let p = parser!((|ch)[LE]);
+    let mut s = pi!(p);
+    let mut seen = HashMap::new();
+
+    for i in 0..1_000_000_000 {
+        if let Some(x) = seen.get(&s) {
+            let diff = i - x;
+            let todo = (1_000_000_000 - i) % diff;
+            let res = x + todo;
+            s = seen.into_iter().find(|(_, v)| *v == res).unwrap().0;
+            break;
+        }
+        seen.insert(s.clone(), i);
+        tilt_north_south(&mut s, -1);
+        tilt_east_west(&mut s, -1);
+        tilt_north_south(&mut s, 1);
+        tilt_east_west(&mut s, 1);
+    }
+    s.ii()
+        .filter(|(x, y)| s[*y][*x] == 'O')
+        .map(|(_, y)| s.len() - y)
+        .s()
+        .save();
+}
+
+fn main() {
+    print!("Part 1: ");
+    one();
+    print!("Part 2: ");
+    two();
+}
