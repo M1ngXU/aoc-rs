ba8db2a80ffcd491aecf37cad34f24ecfa59b9df
MingXu <91617361+M1ngXU@users.noreply.github.com>
2023-12-19 22:48:25 +0100

    05 with hypercuboidsets

--- bin/2023/05/bin.rs
+++ bin/2023/05/bin.rs
@@ -3,7 +3,7 @@
 use aoc_rs::prelude::*;
 
 fn one() {
-    let p = parser!("seeds: " << (pn[" "]) LELE << (((~ "\n" le) << (((pn " " << pn " " <<| pn))[le]))[LELE]));
+    let p = parser!("seeds: " << (pn[" "]) LELE << ((| ((~"\n" > id) >> le) << ((| (pn >> " " pn >> " " pn))[LE]))[LELE]));
     let s = pi!(p);
 
     let (seeds, other) = s;
@@ -34,34 +34,44 @@ fn one() {
 }
 
 fn two() {
-    let p = parser!("seeds: " << (pn[" "]) LELE << (((~ "\n" le) << (((pn " " << pn " " <<| pn))[le]))[LELE]));
+    let p = parser!("seeds: " << (pn[" "]) LELE << ((| ((~"\n" > id) >> le) << ((| (pn >> " " pn >> " " pn))[LE]))[LELE]));
     let s = pi!(p);
 
-    let (seeds, other) = s;
-
-    let location = seeds
+    let (seeds, other): (Vec<isize>, Vec<Vec<(isize, (isize, isize))>>) = s;
+    let other = other
         .into_iter()
-        .tuple_windows()
-        .enumerate()
-        .par_bridge()
-        .into_par_iter()
-        .filter(|(i, _)| i % 2 == 0)
-        .map(|(_, (a, b))| (a, b))
-        .flat_map(|(seedf, seedt)| seedf..seedf + seedt)
-        .map(|mut pos| {
-            for other in &other {
-                for (t, (f, r)) in other.iter() {
-                    if (*f..f + r).contains(&pos) {
-                        pos += t - f;
-                        break;
-                    }
-                }
-            }
-            pos
+        .map(|o| {
+            o.into_iter()
+                .map(|(to, (from, size))| {
+                    (HypercuboidSet::new(vec![[from..from + size]]), to - from)
+                })
+                .collect_vec()
         })
-        .min()
-        .unwrap();
-    location.save();
+        .collect_vec();
+
+    let mut locations = seeds
+        .chunks_exact(2)
+        .map(|c| HypercuboidSet::new(vec![[c[0]..c[0] + c[1]]]))
+        .collect_vec();
+    for o in other {
+        for location in &mut locations {
+            let mut new_location = HypercuboidSet::default();
+            for (map, to) in &o {
+                let mut tmp = &*location & map;
+                *location -= &tmp;
+                for r in &mut tmp.ranges {
+                    r[0] = r[0].starts_at() + to..=r[0].ends_at() + to;
+                }
+                new_location += tmp;
+            }
+            *location += new_location;
+        }
+    }
+    locations
+        .into_iter()
+        .flat_map(|r| r.ranges.into_iter().map(|r| r[0].starts_at()))
+        .mn()
+        .save();
 }
 
 fn main() {
